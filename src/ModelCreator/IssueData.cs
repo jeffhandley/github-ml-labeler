c3dcc2f6d3d0aa0b28e796d0def236f8e966dd26
Jeff Handley <jeffhandley@users.noreply.github.com>
2024-12-27 00:23:58 -0500

    Model Pull Requests. Just use featurized text for the file and folder names.

--- src/ModelCreator/IssueData.cs
+++ src/ModelCreator/IssueData.cs
@@ -1,24 +1,14 @@
 public class Issue
 {
-    [LoadColumn(0)]
     public required uint Number { get; set; }
-
-    [LoadColumn(1)]
     public string? Label { get; set; }
-
-    [LoadColumn(2)]
     public required string Title { get; set; }
-
-    [LoadColumn(3)]
     public string? Body { get; set; }
 }
 
 public class PullRequest : Issue
 {
-    [LoadColumn(4)]
     public string? FileNames { get; set; }
-
-    [LoadColumn(5)]
     public string? FolderNames { get; set; }
 }
 
--- src/ModelCreator/Program.cs
+++ src/ModelCreator/Program.cs
@@ -1,7 +1,6 @@
-﻿global using Microsoft.ML.Data;
-
-using Microsoft.ML;
+﻿using Microsoft.ML;
 using Microsoft.ML.AutoML;
+using Microsoft.ML.Data;
 using Microsoft.ML.Transforms;
 using Microsoft.ML.Transforms.Text;
 
@@ -19,10 +18,10 @@ if (args.Length < 2)
 
 Queue<string> arguments = new(args);
 
-string? issueData = null;
-string? issueModel = null;
-string? pullData = null;
-string? pullModel = null;
+string? issueDataPath = null;
+string? issueModelPath = null;
+string? pullDataPath = null;
+string? pullModelPath = null;
 
 while (arguments.Count > 1)
 {
@@ -31,16 +30,16 @@ while (arguments.Count > 1)
     switch (option)
     {
         case "--issue-data":
-            issueData = arguments.Dequeue();
+            issueDataPath = arguments.Dequeue();
             break;
         case "--issue-model":
-            issueModel = arguments.Dequeue();
+            issueModelPath = arguments.Dequeue();
             break;
         case "--pull-data":
-            pullData = arguments.Dequeue();
+            pullDataPath = arguments.Dequeue();
             break;
         case "--pull-model":
-            pullModel = arguments.Dequeue();
+            pullModelPath = arguments.Dequeue();
             break;
         default:
             ShowUsage();
@@ -54,49 +53,94 @@ if (arguments.Count == 1)
     return;
 }
 
-if ((issueData is not null && issueModel is null) || (issueData is null && issueModel is not null))
+if (((issueDataPath is null) != (issueModelPath is null)) || ((pullDataPath is null) != (pullModelPath is null)))
 {
     ShowUsage();
     return;
 }
 
-if (issueData is not null && issueModel is not null)
+if (issueDataPath is not null && issueModelPath is not null)
 {
-    var mlContext = new MLContext();
-    var columnInference = mlContext.Auto().InferColumns(
-        issueData,
+    var issueContext = new MLContext();
+    var issueColumns = issueContext.Auto().InferColumns(
+        issueDataPath,
         separatorChar: '\t',
         labelColumnIndex: 1,
         hasHeader: true);
-    var loader = mlContext.Data.CreateTextLoader(columnInference.TextLoaderOptions);
-    var data = loader.Load(issueData);
-    var trainTestData = mlContext.Data.TrainTestSplit(data, testFraction: 0.2);
-    var trainer = mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy("LabelKey", "Features");
-    var pipeline = mlContext
+    var issueLoader = issueContext.Data.CreateTextLoader(issueColumns.TextLoaderOptions);
+    var issueData = issueLoader.Load(issueDataPath);
+    var issueDataSets = issueContext.Data.TrainTestSplit(issueData, testFraction: 0.2);
+    var issueTrainer = issueContext.MulticlassClassification.Trainers.SdcaMaximumEntropy("LabelKey", "Features");
+    var issuePipeline = issueContext
         .Transforms.Conversion.MapValueToKey(outputColumnName: "LabelKey", inputColumnName: "Label")
-        .Append(mlContext.Transforms.Text.FeaturizeText(outputColumnName: "TitleFeature", inputColumnName: "Title"))
-        .Append(mlContext.Transforms.Text.FeaturizeText(outputColumnName: "BodyFeature", inputColumnName: "Body"))
-        .Append(mlContext.Transforms.Concatenate(outputColumnName: "Features", "TitleFeature", "BodyFeature"))
-        .Append(trainer)
-        .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+        .Append(issueContext.Transforms.Text.FeaturizeText(outputColumnName: "TitleFeature", inputColumnName: "Title"))
+        .Append(issueContext.Transforms.Text.FeaturizeText(outputColumnName: "BodyFeature", inputColumnName: "Body"))
+        .Append(issueContext.Transforms.Concatenate(outputColumnName: "Features", "TitleFeature", "BodyFeature"))
+        .Append(issueTrainer)
+        .Append(issueContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
 
-    var crossValidation = mlContext.MulticlassClassification.CrossValidate(
-        data: trainTestData.TrainSet,
-        estimator: pipeline,
+    issueContext.MulticlassClassification.CrossValidate(
+        data: issueDataSets.TrainSet,
+        estimator: issuePipeline,
         numberOfFolds: 6,
         labelColumnName: "LabelKey");
 
-    var trainedModel = pipeline.Fit(trainTestData.TrainSet);
-    mlContext.Model.Save(trainedModel, trainTestData.TrainSet.Schema, issueModel);
+    var issueModel = issuePipeline.Fit(issueDataSets.TrainSet);
+    issueContext.Model.Save(issueModel, issueDataSets.TrainSet.Schema, issueModelPath);
 
     var testIssue = new Issue
     {
-        Number = 42,
+        Number = 34,
         Title = "Bug with List<T>",
         Body = "I have encountered a bug when using List<T>. The collection does not work like I expected."
     };
 
-    var engine = mlContext.Model.CreatePredictionEngine<Issue, LabelPrediction>(trainedModel);
+    var engine = issueContext.Model.CreatePredictionEngine<Issue, LabelPrediction>(issueModel);
     var prediction = engine.Predict(testIssue);
-    Console.WriteLine($"Test Issue:\n  Number: {testIssue.Number}\n  Title: {testIssue.Title}\n  Body: {testIssue.Body}\n  LABEL: {prediction.PredictedLabel}");
+    Console.WriteLine($"Test Issue:\n  Number: {testIssue.Number}\n  Title: {testIssue.Title}\n  Body: {testIssue.Body}\n  PREDICTED LABEL: {prediction.PredictedLabel}");
+}
+
+if (pullDataPath is not null && pullModelPath is not null)
+{
+    var pullContext = new MLContext();
+    var pullColumns = pullContext.Auto().InferColumns(
+        pullDataPath,
+        separatorChar: '\t',
+        labelColumnIndex: 1,
+        hasHeader: true);
+    var pullLoader = pullContext.Data.CreateTextLoader(pullColumns.TextLoaderOptions);
+    var pullData = pullLoader.Load(pullDataPath);
+    var pullDataSets = pullContext.Data.TrainTestSplit(pullData, testFraction: 0.2);
+    var pullTrainer = pullContext.MulticlassClassification.Trainers.SdcaMaximumEntropy("LabelKey", "Features");
+    var pullPipeline = pullContext
+        .Transforms.Conversion.MapValueToKey(outputColumnName: "LabelKey", inputColumnName: "Label")
+        .Append(pullContext.Transforms.Text.FeaturizeText(outputColumnName: "TitleFeature", inputColumnName: "Title"))
+        .Append(pullContext.Transforms.Text.FeaturizeText(outputColumnName: "BodyFeature", inputColumnName: "Body"))
+        .Append(pullContext.Transforms.Text.FeaturizeText(outputColumnName: "FileNamesFeature", inputColumnName: "FileNames"))
+        .Append(pullContext.Transforms.Text.FeaturizeText(outputColumnName: "FolderNamesFeature", inputColumnName: "FolderNames"))
+        .Append(pullContext.Transforms.Concatenate(outputColumnName: "Features", "TitleFeature", "BodyFeature", "FileNamesFeature", "FolderNamesFeature"))
+        .Append(pullTrainer)
+        .Append(pullContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
+
+    pullContext.MulticlassClassification.CrossValidate(
+        data: pullDataSets.TrainSet,
+        estimator: pullPipeline,
+        numberOfFolds: 6,
+        labelColumnName: "LabelKey");
+
+    var pullModel = pullPipeline.Fit(pullDataSets.TrainSet);
+    pullContext.Model.Save(pullModel, pullDataSets.TrainSet.Schema, pullModelPath);
+
+    var testPull = new PullRequest
+    {
+        Number = 42,
+        Title = "Fix bug with List<T>",
+        Body = "Fixes #34",
+        FileNames = "List.cs List List.Generic.Tests.cs List.Generic.Tests",
+        FolderNames = "src src/libraries src/libraries/System.Collections src/libraries/System.Collections/tests src/libraries/System.Collections/tests/Generic src/libraries/System.Collections/tests/Generic/List src src/libraries src/libraries/System.Private.CoreLib src/libraries/System.Private.CoreLib/src src/libraries/System.Private.CoreLib/src/System src/libraries/System.Private.CoreLib/src/System/Collections src/libraries/System.Private.CoreLib/src/System/Collections/Generic"
+    };
+
+    var engine = pullContext.Model.CreatePredictionEngine<PullRequest, LabelPrediction>(pullModel);
+    var prediction = engine.Predict(testPull);
+    Console.WriteLine($"Test Issue:\n  Number: {testPull.Number}\n  Title: {testPull.Title}\n  Body: {testPull.Body}\n  FileNames: {testPull.FileNames}\n  {testPull.FolderNames}\n  PREDICTED LABEL: {prediction.PredictedLabel}");
 }
